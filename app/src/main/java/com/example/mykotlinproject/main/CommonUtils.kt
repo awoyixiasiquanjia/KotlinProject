4d03b7925516c3e212e79c86aa48cb496c4714fe
sujinbiao <2236731883@qq.com>
2022-05-13 18:41:54 +0800

    dev_2 第一次

--- app/src/main/java/com/example/mykotlinproject/main/CommonUtils.kt
+++ app/src/main/java/com/example/mykotlinproject/main/CommonUtils.kt
@@ -5,12 +5,17 @@ class CommonUtils {
     //获取返回值
   fun  getname():String{
       //我是master分支上写东西
-
-
       //修改master分支的内容
       //我在dev -1 分支上写东西
       //第二次提交 ，修改一些内容
       //第三次提交
+
+
+
+
+      --------------------
+      第一次修改dev_2的内容
+      
       return ""
     }
 
